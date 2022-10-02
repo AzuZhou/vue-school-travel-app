9804dbe67f4da6a265f64646b3f92e17a840b2ea
Azucena Zhou <abbey-1825@hotmail.com>
2022-10-02 22:31:33 +0200

    add links

--- src/main.js
+++ src/main.js
@@ -1,23 +1,10 @@
 import { createApp } from 'vue'
-import {createRouter, createWebHistory} from 'vue-router'
-import './style.css'
-import App from './App.vue'
-import Home from '@/views/Home.vue'
-import About from '@/views/About.vue'
 
-const router = createRouter({
-    history : createWebHistory(),
-    routes: [{
-        path: "/",
-        name: "Home",
-        component: Home
-    },
-    {
-        path: "/",
-        name: "About",
-        component: About
-    }]
-})
+import router from "@/router"
+
+import App from './App.vue'
+
+import './style.css'
 
 createApp(App)
 .use(router)
