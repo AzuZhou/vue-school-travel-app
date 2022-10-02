414b455f56145dad9981a471058d139e2f861801
Azucena Zhou <abbey-1825@hotmail.com>
2022-10-03 00:38:58 +0200

    dynamic routes

--- src/App.vue
+++ src/App.vue
@@ -2,28 +2,15 @@
 </script>
 
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/brazil">Brazil</router-link>
-    <router-link to="/hawaii">Hawaii</router-link>
-    <router-link to="/jamaica">Jamaica</router-link>
-    <router-link to="/panama">Panama</router-link>
-  </div>
-  <div class="container">
+  <TheNavigation />
+  <div class="container"> 
     <router-view></router-view>
   </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<script>
+import TheNavigation from "@/components/TheNavigation.vue"
+export default {
+  components: {TheNavigation}
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
